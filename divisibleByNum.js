d736322edfb6a7367b44fad123541ab3a5fec3f1
Zain ul Abideen <zainmalik4989@gmail.com>
2024-08-01 20:45:55 +0500

    Is Number divisible by 10

---
+++ divisibleByNum.js
@@ -0,0 +1,11 @@
+//  Return a Boolean if a number is divisible by 10
+
+function divisible(num) {
+  if (num % 10 == 0) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+divisible(30);
+divisible(15);
